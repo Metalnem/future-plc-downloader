4eb96a5b4d635195c31b851846cf547adcd9d902
Metalnem <metalnem@mijailovic.net>
2016-12-09 19:26:08 +0100

    Add credentials handling

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"os"
+
+	"github.com/golang/glog"
+)
+
+var (
+	appKey    = "RymlyxWkRBKjDKsG3TpLAQ"
+	secretKey = "b9dd34da8c269e44879ea1be2a0f9f7c"
+	platform  = "iphone-retina"
+
+	errMissingCredentials = errors.New("Missing email address or password")
+)
+
+func getCredentials() (string, string, error) {
+	email := os.Getenv("EDGE_MAGAZINE_EMAIL")
+	password := os.Getenv("EDGE_MAGAZINE_PASSWORD")
+
+	if email != "" && password != "" {
+		return email, password, nil
+	}
+
+	return "", "", errMissingCredentials
+}
 
 func main() {
-	fmt.Println("Edge Magazine downloader")
+	_, _, err := getCredentials()
+
+	if err != nil {
+		glog.Exit(err)
+	}
 }
